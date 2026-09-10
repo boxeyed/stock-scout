4eaff03fc193d86f44a9b79348cd2a748423ec2c
boxeyed <keertr228@gmail.com>
2026-09-09 18:39:12 -0600

    moved display function to core

--- core.py
+++ core.py
@@ -3,3 +3,7 @@ import sqlite3
 import pandas as pd
 import db.db_handler
 
+def display_db(connection: sqlite3.Connection):
+  """Display DB values through console"""
+  df = pd.read_sql_query("SELECT * FROM securities", connection)
+  print(df)
