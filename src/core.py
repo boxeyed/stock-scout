bf970adfb73a8c8b5be71da18c70fe87bf0f9d94
boxeyed <keertr228@gmail.com>
2026-09-12 14:43:10 -0600

    started writing add security auto function, reevaluate

--- src/core.py
+++ src/core.py
@@ -11,7 +11,13 @@ def add_security_manual(connection: sqlite3.Connection):
   return 0
 
 def add_security_auto(connection: sqlite3.Connection):
-  return 0
+  """Prompts user to input a ticker to fill row with yfinance data."""
+
+  while(True):
+    ticker_input = input("Please enter ticker value to auto-populate database fields: ")
+
+   # Check validity of user input
+
 
 def scout_securities(connection: sqlite3.Connection):
   return 0
@@ -19,3 +25,7 @@ def scout_securities(connection: sqlite3.Connection):
 def main():
   con = dbh.get_connection()
   dbh.setup_db(con)
+  add_security_auto(con)
+
+
+main()
